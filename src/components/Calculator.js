fc9128ae53aef0d1961bafcc2d2197dfd3b80a87
zeff96 <zeffo1975@gmail.com>
2023-03-22 07:45:31 +0300

    Add state and implement handleClick fuction.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,23 @@
 import './Calculator.scss';
+import { useState } from 'react';
 import calculate from '../Logic/Calculate';
 
 const Calculator = () => {
-  
+  const [total, setTotal] = useState('');
+  const [next, setNext] = useState('');
+  const [operation, setOperation] = useState('');
+
+  const handleClick = (e) => {
+    const results = calculate({ total, next, operation }, e.target.textContent);
+    setTotal(results.total);
+    setNext(results.next);
+    setOperation(results.operation);
+  };
   const Digits = () => {
     const nums = [];
     for (let i = 9; i >= 1; i -= 1) {
       nums.push(
-        <button type="button" key={i.toString()} onClick={calculate}>
+        <button type="button" key={i.toString()} onClick={handleClick}>
           {i}
         </button>,
       );
@@ -18,27 +28,47 @@ const Calculator = () => {
   return (
     <div className="calculator">
       <div className="display">
-        <span>0</span>
+        {total || 0}
+        {operation}
+        {next}
       </div>
       <div className="operators">
         <div className="top">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
+          <button type="button" onClick={handleClick}>
+            AC
+          </button>
+          <button type="button" onClick={handleClick}>
+            +/-
+          </button>
+          <button type="button" onClick={handleClick}>
+            %
+          </button>
         </div>
         <div className="side">
-          <button type="button">+</button>
-          <button type="button">*</button>
-          <button type="button">-</button>
-          <button type="button">+</button>
-          <button type="button">=</button>
+          <button type="button" onClick={handleClick}>
+            ÷
+          </button>
+          <button type="button" onClick={handleClick}>
+            x
+          </button>
+          <button type="button" onClick={handleClick}>
+            -
+          </button>
+          <button type="button" onClick={handleClick}>
+            +
+          </button>
+          <button type="button" onClick={handleClick}>
+            =
+          </button>
         </div>
         <div className="digits">
           <Digits />
-          <button className="zero" type="button">
+          <button className="zero" type="button" onClick={handleClick}>
             0
           </button>
-          <button type="button">.</button>
+          <button type="button" onClick={handleClick}>
+            .
+          </button>
         </div>
       </div>
     </div>
