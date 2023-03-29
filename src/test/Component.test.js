648a24760e4b2135cccadbc7492f4cc7ffe6102b
zeff96 <zeffo1975@gmail.com>
2023-03-29 18:51:49 +0300

    Add update to the quotes.

--- src/test/Component.test.js
+++ src/test/Component.test.js
@@ -1,24 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import { Route, MemoryRouter } from 'react-router-dom';
-import Calculator from '../components/Calculator';
-import HomePage from '../components/Home';
-import Navlinks from '../components/Navbar';
-import Quotes from '../components/Quote';
+import { render, screen } from "@testing-library/react";
+import { Route, MemoryRouter } from "react-router-dom";
+import Calculator from "../components/Calculator";
+import HomePage from "../components/Home";
+import Navlinks from "../components/Navbar";
+import Quotes from "../components/Quote";
 
-describe('component test', () => {
-  it('render home title', () => {
+describe("component test", () => {
+  it("render home title", () => {
     render(<HomePage />);
     const header = screen.getByText(/Welcome to our page!/i);
     expect(header).toMatchSnapshot();
   });
 
-  it('render calculator title', () => {
+  it("render calculator title", () => {
     render(<Calculator />);
     const header = screen.getByText(/Let's do some maths!/i);
     expect(header).toMatchSnapshot();
   });
 
-  it('render Navlink title', () => {
+  it("render Navlink title", () => {
     <Route>
       render(
       <Navlinks />
@@ -26,19 +26,18 @@ describe('component test', () => {
       expect(header).toMatchSnapshot();
     </Route>;
   });
-  it('simulate user click event', () => {
+  it("simulate user click event", () => {
     <MemoryRouter>
       render(
-      <Navlinks />
-      ) const linkEl = screen.getByText(/home/i);
+      <Navlinks />) const linkEl = screen.getByText(/home/i);
       fireEvent.click(linkEl); expect(getByText(/Welcome to our
       page!/i)).toBeInTheDocument();
     </MemoryRouter>;
   });
 
-  it('renders a div element with class name "quotes"', async () => {
+  it('renders a div element with class name "quotes"', () => {
     render(<Quotes />);
-    const divElement = await screen.getByTestId('quotes');
+    const divElement = screen.getByTestId("quotes");
 
     expect(divElement).toMatchSnapshot();
   });
